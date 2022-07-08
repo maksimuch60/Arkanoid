ec873d2583636eb9ba29fe7d760fa027176d2a51
Maksim Anishchenko <maksimych60@gmail.com>
2022-07-08 20:47:11 +0300

    add full pause

---
+++ Assets/Scripts/GameScreenManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+
+public class GameScreenManager : MonoBehaviour
+{
+    #region Variables
+
+    [SerializeField] private GameObject _gameScreen;
+    [SerializeField] private GameObject _gameOverScreen;
+    [SerializeField] private GameObject _pauseScreen;
+
+    private GameObject[] _windows;
+
+    #endregion
+
+
+    #region Unity lifecycle
+
+    private void Awake()
+    {
+        _windows = new[] {_gameScreen, _gameOverScreen, _pauseScreen};
+    }
+
+    #endregion
+
+
+    #region Public methods
+
+    public void ChangeWindow()
+    {
+        
+    }
+
+    #endregion
+}
